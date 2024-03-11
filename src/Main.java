1347eb2c1eadeceabb0f26df39e8f83a1a9b8c4d
Artem <artem.yaremenko.1992@gmail.com>
2024-03-12 02:30:51 +0300

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         //Второе задание
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
@@ -17,6 +23,17 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        //Четвертое задание
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Пятое задание
+
 
     }
 }
